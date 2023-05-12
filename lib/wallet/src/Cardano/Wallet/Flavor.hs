673292b8fe77bbcad0516ed1c1fd304feab7321b
paolino <paolo.veronelli@gmail.com>
2023-05-12 13:42:15 +0000

    Add key flavors.

--- lib/wallet/src/Cardano/Wallet/Flavor.hs
+++ lib/wallet/src/Cardano/Wallet/Flavor.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Cardano.Wallet.Flavor
@@ -10,6 +14,11 @@ module Cardano.Wallet.Flavor
     , WalletFlavor (..)
     , KeyOf
     , TestState (..)
+    , KeyFlavor (..)
+    , KeyFlavorS (..)
+    , StateWithAnyKey
+    , keyFlavorOfState
+    , StateWithKey
     )
 where
 
@@ -75,3 +84,31 @@ type family KeyOf (s :: Type) :: (Depth -> Type -> Type) where
     KeyOf (SeqAnyState n k p) = k
     KeyOf (RndAnyState n p) = ByronKey
     KeyOf (TestState s k) = k
+
+data KeyFlavorS a where
+    ByronKeyS :: KeyFlavorS ByronKey
+    IcarusKeyS :: KeyFlavorS IcarusKey
+    ShelleyKeyS :: KeyFlavorS ShelleyKey
+    SharedKeyS :: KeyFlavorS SharedKey
+
+class KeyFlavor a where
+    keyFlavor :: KeyFlavorS a
+
+instance KeyFlavor ByronKey where
+    keyFlavor = ByronKeyS
+
+instance KeyFlavor IcarusKey where
+    keyFlavor = IcarusKeyS
+
+instance KeyFlavor ShelleyKey where
+    keyFlavor = ShelleyKeyS
+
+instance KeyFlavor SharedKey where
+    keyFlavor = SharedKeyS
+
+type StateWithAnyKey s = KeyFlavor (KeyOf s)
+
+keyFlavorOfState :: StateWithAnyKey s => KeyFlavorS (KeyOf s)
+keyFlavorOfState = keyFlavor
+
+type StateWithKey s k = (StateWithAnyKey s, KeyOf s ~ k)
