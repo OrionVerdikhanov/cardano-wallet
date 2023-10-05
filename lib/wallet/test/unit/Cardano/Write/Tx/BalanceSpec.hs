4741efa15b613b9767ad8cd7547fbf7e79f2f852
Jonathan Knowles <mail@jonathanknowles.net>
2023-10-05 09:08:05 +0000

    WIP

--- lib/wallet/test/unit/Cardano/Write/Tx/BalanceSpec.hs
+++ lib/wallet/test/unit/Cardano/Write/Tx/BalanceSpec.hs
@@ -848,12 +848,6 @@ newtype TxBalanceSurplus a = TxBalanceSurplus {unTxBalanceSurplus :: a}
 mkGen :: (QCGen -> a) -> Gen a
 mkGen f = MkGen $ \g _ -> f g
 
-instance MonadRandom Gen where
-    getRandom = mkGen (fst . random)
-    getRandoms = mkGen randoms
-    getRandomR range = mkGen (fst . randomR range)
-    getRandomRs range = mkGen (randomRs range)
-
 data Wallet' = Wallet' UTxOAssumptions UTxO AnyChangeAddressGenWithState
     deriving Show via (ShowBuildable Wallet')
 
