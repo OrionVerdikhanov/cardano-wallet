7f07e2642fae00ffd04fce4fbd4a7bd91311ff06
paolino <paolo.veronelli@gmail.com>
2024-06-05 14:56:56 +0000

    Tweaks to allow 9.8.2 compilation

--- lib/benchmarks/exe/db-bench.hs
+++ lib/benchmarks/exe/db-bench.hs
@@ -16,6 +16,7 @@
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
 
 #if __GLASGOW_HASKELL__ >= 902
 {-# OPTIONS_GHC -Wno-ambiguous-fields #-}
--- lib/local-cluster/lib/Cardano/Wallet/Launch/Cluster/CommandLine.hs
+++ lib/local-cluster/lib/Cardano/Wallet/Launch/Cluster/CommandLine.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+{-# OPTIONS_GHC -Wno-x-partial #-}
+
 module Cardano.Wallet.Launch.Cluster.CommandLine
     ( CommandLineOptions (..)
     , parseCommandLineOptions
