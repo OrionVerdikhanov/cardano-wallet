2fb01658a74043631334b03f1bb33c6bcd03a01a
Github Action <gha@cardanofoundation.org>
2024-06-13 19:03:58 +0000

    Add "Build linux package" step

--- scripts/buildkite/release/linux-e2e.sh
+++ scripts/buildkite/release/linux-e2e.sh
@@ -1,7 +1,23 @@
-#! /usr/bin/env -S nix shell 'nixpkgs#rsync' --command bash
+#! /usr/bin/env -S nix shell 'nixpkgs#rsync' 'nixpkgs#gnutar' --command bash
 # shellcheck shell=bash
 
 set -euox pipefail
+
+RELEASE_CANDIDATE_BRANCH=$(buildkite-agent meta-data get "release-candidate-branch")
+echo "RELEASE_CANDIDATE_BRANCH=$RELEASE_CANDIDATE_BRANCH"
+
+VERSION=$(buildkite-agent meta-data get "release-version")
+echo "VERSION=$VERSION"
+
+
+buildkite-agent artifact \
+    download "result/linux/cardano-wallet-$VERSION-linux64.tar.gz" "."
+
+
+tar xvzf "result/linux/cardano-wallet-$VERSION-linux64.tar.gz"
+
+TESTS_E2E_BINDIR="$(pwd)/cardano-wallet-$VERSION-linux64"
+export TESTS_E2E_BINDIR
 
 cd test/e2e
 
@@ -12,9 +28,8 @@ mkdir -p "$TESTS_NODE_DB"/preprod
 rsync -a "$NODE_STATE_DIR/db-new" "$TESTS_NODE_DB/preprod"
 
 git fetch --all
-# RELEASE_CANDIDATE_BRANCH=$(buildkite-agent meta-data get "release-candidate-branch")
-RELEASE_CANDIDATE_BRANCH="release-candidate-new/v$(date +%Y-%m-%d)"
-echo "RELEASE_CANDIDATE_BRANCH=$RELEASE_CANDIDATE_BRANCH"
+
+
 
 git checkout "$RELEASE_CANDIDATE_BRANCH"
 
@@ -26,6 +41,13 @@ tmpfile=$(mktemp /tmp/node-preprod.XXXXXX)
 CARDANO_NODE_SOCKET_PATH="$tmpfile"
 export CARDANO_NODE_SOCKET_PATH
 
-nix-shell --run "./run_all_tests.sh"
+
+TESTS_E2E_STATEDIR=./state
+export TESTS_E2E_STATEDIR
+
+TESTS_E2E_TOKEN_METADATA=https://metadata.world.dev.cardano.org/
+export TESTS_E2E_TOKEN_METADATA
+
+nix-shell --run "rake run_on[preprod,sync,true]"
 
 rm "$tmpfile"
